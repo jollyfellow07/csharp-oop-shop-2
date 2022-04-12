426323c80dc8b8f62a0501a57a1bf1c1b3913479
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-12 14:09:17 +0200

    Add project files.

---
+++ CShop2/Prodotto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShop2
+{
+    internal class Prodotto
+    {
+        private int codice;
+        private string name;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+        public Prodotto(string name, string descrizione, double prezzo, double iva)
+        {
+
+            this.name = name;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+            Random codiceRandom = new Random();
+            codice = codiceRandom.Next(100000000);
+
+        }
+
+        /* METODO: ASSEGNO AL CODICE*/
+        public int GetCodice()
+        {
+            return codice;
+        }
+
+        /*bonus*/
+        public string padleft()
+        {
+
+            string strvalue = Convert.ToString(codice).PadLeft(8, '0');
+            return strvalue;
+        }
+
+        /*STAMPA TUTTE LE CARATTERISTICHE DEL MIO PRODOTTO*/
+        public void stampa()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\n**************PRODOTTO***************");
+            Console.WriteLine("Il nome del prodotto è: " + name);
+            Console.WriteLine("La descrizione del mio prodotto: " + descrizione);
+            Console.WriteLine("il codice è: " + padleft());
+            Console.WriteLine("il prezzo senza iva è: " + prezzo + "€");
+            Console.WriteLine("L'iva del prodotto è: " + iva + "%");
+            Console.WriteLine("Il prezzo del prodotto con iva è: " + CalcoloPrezzoConIva() + "€");
+            Console.WriteLine("**************************************");
+        }
+
+        /*METODO: PER CALCOLARE IL PREZZO CALCOLATO CON IVA*/
+
+        public double CalcoloPrezzoConIva()
+        {
+            double prezzoFinale;
+            if (iva == 22)
+            {
+                prezzoFinale = prezzo * 1.22;
+
+            }
+            else if (iva == 5)
+            {
+                prezzoFinale = prezzo * 1.05;
+
+            }
+            else
+            {
+                prezzoFinale = prezzo * 1.10;
+            }
+            prezzoFinale = Math.Round(prezzoFinale, 2);
+            return prezzoFinale;
+        }
+
+
+
+
+    }
+}
---
+++ CShop2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CShop2;
+
+Prodotto mela = new Prodotto("MELA", "questa mela è rossa", 0.50, 5);
+int codice = mela.GetCodice();
+mela.stampa();
+
+Prodotto telefono = new Prodotto("iphone", "Apple iPhone 13 è l’ultima uscita di casa Apple, apparentemente simile al suo predecessore, ma in realtà molto innovativo per display, batteria e fotocamera. ", 1000, 22);
+int iphoneCodice = telefono.GetCodice();
+telefono.stampa();
+
+Prodotto medicina = new Prodotto("tachipirina", "serve per la febbre", 10.20, 10);
+int medicinaCodice = medicina.GetCodice();
+medicina.stampa();
+
+Prodotto sedia = new Prodotto("Sedia", "di legno", 50, 22);
+int sediaCodice = sedia.GetCodice();
+sedia.stampa();
+
